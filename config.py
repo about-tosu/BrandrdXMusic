d4498fe54432e93b4f5702ca403363f39023f1fd
soulxmusicbot <rustydragon20@gmail.com>
2024-06-15 09:30:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002198719573"))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID". "7013604559"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
